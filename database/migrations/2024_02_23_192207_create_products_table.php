46347d3d3bab095d8bc6302e84e4feffe9d02871
afmurillo97 <afmurillo97@gmail.com>
2024-02-23 20:11:11 -0500

    laravel 10 POS first commit

---
+++ database/migrations/2024_02_23_192207_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('code');
+            $table->string('name');
+            $table->integer('stock');
+            $table->string('description');
+            $table->string('image');
+            $table->enum('status', [1, 0])->nullable();
+            $table->timestamps();
+
+            // Define restriction
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
