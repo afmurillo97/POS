0004f4ad8e2fcf893fc0e380055e8af79801d28c
afmurillo97 <afmurillo97@gmail.com>
2024-03-06 11:14:04 -0500

    new model for Products

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'category_id',
+        'code',
+        'name',
+        'stock',
+        'description',
+        'image',
+        'status'
+    ];
+
+    protected $guarded = [
+        
+    ];
+}
