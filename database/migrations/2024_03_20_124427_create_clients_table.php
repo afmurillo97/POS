7f14fb2b7b52568eaf6401657183dba4928d5b00
afmurillo97 <afmurillo97@gmail.com>
2024-03-20 20:42:05 -0500

    migration for clients table

---
+++ database/migrations/2024_03_20_124427_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('client_type');
+            $table->string('id_type');
+            $table->string('id_number');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->enum('status', [1, 0])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
