0775c4e1a1e958a8858a7290448ad0c3cd1cc70d
afmurillo97 <afmurillo97@gmail.com>
2024-03-22 18:35:00 -0500

    new migration for providers

---
+++ database/migrations/2024_03_22_173150_create_providers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('id_type');
+            $table->string('id_number');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->enum('status', [1, 0])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('providers');
+    }
+};
