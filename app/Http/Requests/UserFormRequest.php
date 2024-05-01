213b72edd1174171cdba05fd9bcb508fb9a61112
afmurillo97 <afmurillo97@gmail.com>
2024-04-30 19:14:16 -0500

    new form request for user requests

---
+++ app/Http/Requests/UserFormRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserFormRequest extends FormRequest
+{
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'name' => ucwords($this->name), // Every word start in UPPER
+        ]);
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $passwordRule = ($this->getMethod() === 'PUT') ? 'nullable' : 'required';
+
+        return [
+
+            'role' => 'required',
+            'name' => 'required|max:100',
+            'email' => 'required|email',
+            'password' => $passwordRule . '|min:6'
+
+        ];
+    }
+}
