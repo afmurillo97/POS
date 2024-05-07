b417ee569a624b0ddc3822ad0f30a90042baa33b
afmurillo97 <afmurillo97@gmail.com>
2024-05-06 21:45:42 -0500

    new routes for roles and permissions

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,6 +34,19 @@
     <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
+  @php
+      $userRole = null;
+      if (auth()->check()) {
+          if (auth()->user()->hasRole('Administrator')) {
+              $userRole = 'Administrator';
+          } elseif (auth()->user()->hasRole('User')) {
+              $userRole = 'User';
+          } else {
+              $userRole = 'No Rol';
+          }
+      }
+  @endphp
+
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -41,31 +54,19 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
 
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-      </li>
+      @if (auth()->check())
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
-              <i class="far fa-user"></i>
+          {{ auth()->user()->name }} <i class="far fa-user"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
-              <span class="dropdown-item dropdown-header">User Settings</span>
+              <span class="dropdown-item dropdown-header">User Settings: {{ $userRole }}</span>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="{{ route('logout') }}"
                   onclick="event.preventDefault();
@@ -83,6 +84,7 @@
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
       </li>
+      @endif
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -90,7 +92,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/home" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">POINT OF SALE</span>
     </a>
@@ -199,9 +201,15 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('roles.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Roles</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('permissions.index') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Permissions</p>
                 </a>
               </li>
             </ul>
