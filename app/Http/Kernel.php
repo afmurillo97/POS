35186e93d5b95f0a59b5bfff9504ea98f24aa1f2
afmurillo97 <afmurillo97@gmail.com>
2024-05-06 21:44:36 -0500

    add roles and permissions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProviderController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -26,39 +28,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Export any file
-Route::post('/export/csv', [ExportController::class, 'csv'])->name('export.csv');
-Route::post('/export/excel', [ExportController::class, 'excel'])->name('export.excel');
-Route::post('/export/pdf', [ExportController::class, 'pdf'])->name('export.pdf');
-
-// Categories
-Route::resource('depot/categories', CategoryController::class);
-Route::put('depot/categories/{category}/toggle', [CategoryController::class, 'toggle'])->name('categories.toggle');
-
-// Products
-Route::resource('depot/products', ProductController::class);
-Route::put('depot/products/{product}/toggle', [ProductController::class, 'toggle'])->name('products.toggle');
-
-// Clients
-Route::resource('sales/clients', ClientController::class);
-Route::put('sales/clients/{client}/toggle', [ClientController::class, 'toggle'])->name('clients.toggle');
-
-// Providers
-Route::resource('shopping/providers', ProviderController::class);
-Route::put('shopping/providers/{provider}/toggle', [ProviderController::class, 'toggle'])->name('providers.toggle');
-
-// Incomes
-Route::resource('shopping/incomes', IncomeController::class);
-Route::put('shopping/incomes/{income}/toggle', [IncomeController::class, 'toggle'])->name('incomes.toggle');
-
-// Sales
-Route::resource('sales/sales', SaleController::class);
-Route::put('sales/sales/{sale}/toggle', [SaleController::class, 'toggle'])->name('sales.toggle');
-
 // Authentication
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Users
-Route::resource('security/users', UserController::class);
-Route::put('security/users/{user}/toggle', [UserController::class, 'toggle'])->name('users.toggle');
+Route::middleware([
+    'auth',
+])->group(function () {
+    // Home
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    // Security
+    Route::resource('security/roles', RoleController::class);
+    Route::resource('security/permissions', PermissionController::class);
+    Route::resource('security/users', UserController::class);
+    Route::put('security/users/{user}/toggle', [UserController::class, 'toggle'])->name('users.toggle');
+
+    // Export any file
+    Route::post('/export/csv', [ExportController::class, 'csv'])->name('export.csv');
+    Route::post('/export/excel', [ExportController::class, 'excel'])->name('export.excel');
+    Route::post('/export/pdf', [ExportController::class, 'pdf'])->name('export.pdf');
+
+    // Categories
+    Route::resource('depot/categories', CategoryController::class);
+    Route::put('depot/categories/{category}/toggle', [CategoryController::class, 'toggle'])->name('categories.toggle');
+
+    // Products
+    Route::resource('depot/products', ProductController::class);
+    Route::put('depot/products/{product}/toggle', [ProductController::class, 'toggle'])->name('products.toggle');
+
+    // Clients
+    Route::resource('sales/clients', ClientController::class);
+    Route::put('sales/clients/{client}/toggle', [ClientController::class, 'toggle'])->name('clients.toggle');
+
+    // Providers
+    Route::resource('shopping/providers', ProviderController::class);
+    Route::put('shopping/providers/{provider}/toggle', [ProviderController::class, 'toggle'])->name('providers.toggle');
+
+    // Incomes
+    Route::resource('shopping/incomes', IncomeController::class);
+    Route::put('shopping/incomes/{income}/toggle', [IncomeController::class, 'toggle'])->name('incomes.toggle');
+
+    // Sales
+    Route::resource('sales/sales', SaleController::class);
+    Route::put('sales/sales/{sale}/toggle', [SaleController::class, 'toggle'])->name('sales.toggle');
+});
