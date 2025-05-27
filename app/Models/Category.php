c1acb7b073e4245b56ffef08b554c28173d70c9c
afmurillo97 <afmurillo97@gmail.com>
2025-05-27 00:28:30 +0000

    Add products relationship to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -24,4 +25,12 @@ class Category extends Model
     protected $guarded = [
         
     ];
+    
+    /**
+     * Get the products for the category.
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
